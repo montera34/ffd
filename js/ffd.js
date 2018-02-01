a2e0d0ccc7d3622aec5c6bdb9974d7498d281951
skotperez <voraginebramante@gmail.com>
2018-02-01 07:41:19 +0100

    Adds auto-fill fields for sign up form

--- js/ffd.js
+++ js/ffd.js
@@ -1,15 +1,25 @@
 (function($) {
 
-	var selectOut = '<select id="price" name="price"><option></option></select>'
 	$(document).ready(function(){
-// transform price input field in select box
-		$('input#price').parent('p').append(selectOut);
+
+		// transform price input field in select box
+		$('input#price').parent('p').append('<select id="price" name="price"><option></option></select>');
 		$('input#price').remove();
 		for (var p = 2; p < 51; p++) {
 			if ( p == 2 ) { price = ffdBasePrice * 2; }
 			else { price = ffdReducedPrice * p; }
 			$('select#price').append('<option value="'+price+'">'+p+' pilotes: '+price+'€</option>');
 		}
+
+		// automatic fill in fields
+		$('input[name="_user_pilot_equal_contact"]').change(function() {
+		        if($(this).is(":checked")) {
+				lastname = $('input[name="_user_legal_contact_lastname"]').val();
+				firstname = $('input[name="_user_legal_contact_firstname"]').val();
+				$('#_user_pilot_1_lastname').val(lastname);
+				$('#_user_pilot_1_firstname').val(firstname);
+			}
+		})
 	});
 
 })(jQuery);
